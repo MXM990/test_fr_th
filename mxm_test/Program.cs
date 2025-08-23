be4b8b74df5ca28f0c22944afc8c60e86a007063
morerise <morerise@gmail.com>
2025-08-23 13:51:08 +0300

    Add project files.

---
+++ mxm_test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mxm_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("add other line like 123");
+        }
+    }
+}
